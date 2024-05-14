b17e1500a90ac6e08f2a13e8007f863e540b9840
Johannes Snildal <johannes.snildal@gmail.com>
2024-05-14 11:59:09 +0200

    projects page hero text improvements

--- app/(main)/(home)/projects/page.tsx
+++ app/(main)/(home)/projects/page.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 const ProjectsPage = () => {
   return (
     <section>
-      <h1 className='font-extrabold font-serif text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>Projects</h1>
+      <div className='text-center flex flex-col gap-2 max-w-prose mx-auto'>
+        <h1 className='font-extrabold font-serif text-5xl md:text-6xl lg:text-7xl'>Projects</h1>
+        <p className='text-zinc-400 font-light text-md md:text-lg lg:text-xl'>A brief overview of my recent projects, tech-stacks used and links to each individual project!</p>
+      </div>
     </section>
   )
 }
