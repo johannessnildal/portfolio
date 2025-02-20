0d21f1bd3edcd3b7cc7b0f99126d2bca122fcdc0
johannes <johannes.snildal@gmail.com>
2025-02-20 22:54:36 +0100

    debugging showed it was not the background, but the navbar elements causing the issue - reverting

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -5,9 +5,19 @@ import React, { ReactNode } from 'react'
 const Mainlayout = ({ children }: { children: ReactNode }) => {
   return (
     <main className='relative h-full w-full'>
+      <div 
+      className="absolute z-[-2] overflow-hidden bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_20%,transparent_100%)]"
+      >
+      </div>
+      <div
+      className='absolute z-[-1] overflow-hidden top-0 mx-auto from-zinc-500 to-zinc-600 opacity-50'
+      >
+      </div>
+        <div className='relative'>
           <NavBar />
           {children}
           <Footer />
+        </div>
     </main>
   )
 }
