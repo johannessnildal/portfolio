cfc4de175ecab91a7460753872b23a3b4a3c2015
johannes <johannes.snildal@gmail.com>
2025-02-24 22:00:34 +0100

    added more sub-titles for hero section

--- components/hero-descriptions.tsx
+++ components/hero-descriptions.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 
-const descriptions = ["Websites", "Web Apps", "Experiences", "Interfaces"];
+const descriptions = ["Websites", "Web Apps", "Experiences", "Interfaces", "Interactions", "Designs"];
 
 const DescriptionCarousel: React.FC = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
